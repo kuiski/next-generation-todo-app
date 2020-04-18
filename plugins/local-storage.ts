585d2ac21c9cba391097e4e373731389c7783692
Ubuntu <ubuntu@ip-172-31-25-148.ap-northeast-1.compute.internal>
2020-04-18 08:51:29 +0000

    export initial app

---
+++ plugins/local-storage.ts
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import {Todo} from '~/types/todo'
+
+export interface TodoStorage {
+  fetch: () => Todo[]
+  save: (todos: Todo[]) => void
+}
+
+const VueLocalStorage = {
+  install: (_Vue: any, _options: any) => {
+    const STORAGE_KEY = 'todos-vuejs-demo'
+    const todoStorage: TodoStorage = {
+      fetch: () => {
+        const todos: Todo[] = JSON.parse(
+            localStorage.getItem(STORAGE_KEY) || '[]'
+        )
+        todos.forEach((todo, index) => {
+            todo.id = index
+        })
+        
+        return todos
+      },
+      save: function(todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+      }
+    }
+
+    Vue.prototype.$localStorage = todoStorage
+  }
+}
+
+Vue.use(VueLocalStorage)
---
+++ types/todo.ts
@@ -0,0 +1,6 @@
+export interface Todo {
+  id: number,
+  comment: string,
+  state: number
+}
+
