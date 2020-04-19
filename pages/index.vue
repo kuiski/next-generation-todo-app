0e47215f95c8815e42371c3738cdd9de0bf4a4b1
osa9 <>
2020-04-19 15:00:28 +0900

    create index

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,33 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        tutorial
-      </h1>
-      <h2 class="subtitle">
-        My priceless Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1 class="title">次世代のTodoアプリ</h1>
+    <ul>
+      <li><nuxt-link to="/todo">Todo</nuxt-link></li>
+      <li><nuxt-link to="/todo-old-generation">前時代のTodo</nuxt-link></li>
+      <li><nuxt-link to="/todo-composition">新世代のTodo</nuxt-link></li>
+    </ul>
   </div>
 </template>
 
+<style scoped>
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 50px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+</style>
+
 <script lang="ts">
 import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
 
 export default Vue.extend({
   components: {
-    Logo
   }
 })
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
